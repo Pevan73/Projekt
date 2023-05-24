4356d2d5a273b17e030b0afafe8ec17098b9738c
Pevan73 <89877164+Pevan73@users.noreply.github.com>
2023-05-24 15:28:05 +0200

    Upraveno dle konzultace
    Upraveno dle konzultace, odstraněny zbytečné řádky

--- main.py
+++ main.py
@@ -8,26 +8,24 @@ while True:
         prijmeni = input("Příjmení: ")
         vek = input("Věk: ")
         telefon = input("Telefonní číslo: ")
-        pojisteny = Pojisteni(jmeno, prijmeni, vek, telefon)
-        Pojisteni.pridej_pojisteneho(pojisteny)
+        Pojisteni.pridej_pojisteneho(Pojisteni(jmeno, prijmeni, vek, telefon))
         print("Pojištěný byl přidán.")
-        input("Stiskněte libovolnou klávesu pro pokračování...")
+    
     elif volba == "2":
         Pojisteni.zobraz_seznam()
-        input("Stiskněte libovolnou klávesu pro pokračování...")
+        
     elif volba == "3":
         jmeno = input("Jméno: ")
         prijmeni = input("Příjmení: ")
         pojisteny = Pojisteni.vyhledej_pojisteneho(jmeno, prijmeni)
         if pojisteny:
             print(pojisteny)
-            input("Stiskněte libovolnou klávesu pro pokračování...")
         else:
             print("Pojištěný nebyl nalezen.")
-            input("Stiskněte libovolnou klávesu pro pokračování...")
+            
     elif volba == "4":
         id_pojisteneho= input("Zadejte ID pojištěného, které chcete smazat:\n")
         Pojisteni.smaz_pojisteneho(id_pojisteneho)
-        input("Stiskněte libovolnou klávesu pro pokračování...")
+    
     elif volba == "5":
         break
